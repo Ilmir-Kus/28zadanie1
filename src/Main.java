66db570f3c878d8425bf61314cd69d3c944b0258
Neto test <ilmir.kusmaev03@mail.ru>
2023-01-31 09:53:06 +0400

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int ticket = 3500; //Стоимость билета
-        int bonus = ticket/20; //Кол-во милей
+        int bonus = ticket / 20; //Кол-во милей
         System.out.println("Кол-во миллей:" + bonus);
     }
 }
