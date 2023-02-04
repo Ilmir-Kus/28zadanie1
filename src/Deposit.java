32c1758b9a5d06442b6d5ed53252e7a46d143b63
Neto test <ilmir.kusmaev03@mail.ru>
2023-02-04 16:02:16 +0400

    0402

--- src/Deposit.java
+++
@@ -1,5 +0,0 @@
-public class Deposit {
-    public static void main(String[] args) {
-
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 3500; //Стоимость билета
-        int bonus = ticket / 20; //Кол-во милей
-        System.out.println("Кол-во миллей:" + bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 3500;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
